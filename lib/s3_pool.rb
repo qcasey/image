64ff1bf5f852e70b61cae3010b863ebd9feb5465
Quinn Casey <qcasey1@gmail.com>
2021-02-08 08:07:27 -0800

    Update s3_pool.rb

--- lib/s3_pool.rb
+++ lib/s3_pool.rb
@@ -3,6 +3,9 @@ S3_POOL = ConnectionPool.new(size: 10, timeout: 5) do
     provider: "AWS",
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    persistent: true
+    persistent: true,
+    host: ENV['ENTRY_IMAGE_HOST'],
+    endpoint: ENV['AWS_S3_ENDPOINT'],
+    path_style: true,
   )
 end
